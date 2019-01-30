503fd82eb2970233ea94f002f8f8231a9e63f524
MasayaKataoka <masaya.kataoka@tier4.jp>
2019-01-30 13:33:49 +0900

    add Editor plugin.

---
+++ Assets/Editor/UnityVectorMapLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("UnityVectorMapLoader/Load")]
+    public static void OpenVectorMapDir()
+    {
+
+        if (Application.platform == RuntimePlatform.OSXEditor)
+        {
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
+        }
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            EditorUtility.RevealInFinder(Application.persistentDataPath);
+            Debug.Log("ShowInExplorer:" + Application.persistentDataPath);
+        }
+
+    }
+}
